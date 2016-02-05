5dfadbf0bd3c6892a3753f085b099ca798a27f2b
zakiul <2sunnymail@gmail.com>
2016-02-04 23:54:09 -0500

    Project files added

---
+++ TestWindowsService/Scheduler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TestWindowsService
+{
+    public partial class Scheduler : ServiceBase
+    {
+        private Timer timer1 = null;
+
+
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1 = new Timer();
+            this.timer1.Interval = 30000;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Tick);
+            timer1.Enabled = true;
+            Library.WriteErrorLog("Test window service started");
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+            Library.WriteErrorLog("Test window service stopped");
+        }
+
+        private void timer1_Tick(object sender, ElapsedEventArgs e)
+        {
+            Library.WriteErrorLog("Timer ticked and some job has been done successfully");
+        }
+    }
+}
